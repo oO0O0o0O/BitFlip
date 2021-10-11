1e4ef5f52b41638774f9e73d6d7ba8c430fee308
Jack Baynham <92075035+JackBaynhamGH@users.noreply.github.com>
2021-10-11 09:55:46 -0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Accepts a filename and generates a copy with all the bits flipped. Useful for 
+deactivating and safely transfering malware. Output file: out.txt
+
+Format:
+py flip.py [filename]
---
+++ flip.py
@@ -0,0 +1,27 @@
+'''
+Generates a copy on the input file with the bits flipped.
+'''
+
+from sys import argv
+
+def main():
+    # Load source data
+    srcFile = open(argv[1], 'rb')
+    data = bytearray(srcFile.read())
+
+    # Flip Bits
+    for i in range(len(data)):
+        if (~data[i] < 0):
+            data[i] = ~data[i] + 256
+        else:
+            data[i] = ~data[i]
+
+    # Save output data
+    outFile = open('out.txt', 'wb')
+    outFile.write(data)
+
+    # Cleanup
+    srcFile.close()
+    outFile.close()
+
+main()
